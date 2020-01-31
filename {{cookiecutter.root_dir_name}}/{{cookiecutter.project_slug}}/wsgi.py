8e270304dab4cb9430358e50d867aa7eb1071da2
prasanta <prasantakakati1994@gmail.com>
2020-02-01 00:03:33 +0530

    [FIXED] poetry needed non spaced project name

--- {{cookiecutter.root_dir_name}}/{{cookiecutter.project_slug}}/wsgi.py
+++ {{cookiecutter.root_dir_name}}/{{cookiecutter.project_slug}}/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                      '{{cookiecutter.project_slug}}.settings')
+                      '{{cookiecutter.project_slug}}.settings.development')
 
 application = get_wsgi_application()
